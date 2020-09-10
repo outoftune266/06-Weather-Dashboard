d431ea3ec39ac5602c4826cc1a78f80086b267f8
Zak Monnet <zakmonnet@gmail.com>
2020-09-10 10:46:10 -0500

    added intial HTML content and styling

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+//Global Variables
+
+//Takes search input and gets Lat/Long to pass to getWeather fuction
+function getLocation() {
+
+};
+
+//Gets weather info for specified Lat/Long
+function getWeather() {
+
+};
+
